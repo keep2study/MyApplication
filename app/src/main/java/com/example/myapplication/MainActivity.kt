df53db55049647da4fc288dc231686d5918c04de
user <user@mail>
2022-03-30 14:03:43 +0800

    Button Done
    完成按鈕功能
    fafewafwafewa

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         var myT1: TextView =findViewById<TextView>(R.id.t1)
         myT1.text="Hello !!!"
+
+        var btn1=findViewById<Button>(R.id.button1)
 
     }
 }
