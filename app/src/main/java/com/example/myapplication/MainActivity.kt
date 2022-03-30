c500431c8425905ad70358ce7480febb0e2bbd12
user <user@mail>
2022-03-30 13:53:55 +0800

    show hello
    顯示Hello的功能己經完成
    fdfdsafdsa
    fafewafwafewa

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var myT1: TextView =findViewById<TextView>(R.id.t1)
+        myT1.text="Hello !!!"
+
+    }
+}
